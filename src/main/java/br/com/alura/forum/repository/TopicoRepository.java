0d9835000f8c9159a86d5d275798eee1a9fb40c6
Maxwell Barros <maxwellbarros@yahoo.com.br>
2022-05-23 13:38:30 -0300

    Implementando o cadastro de Topicos

--- src/main/java/br/com/alura/forum/repository/TopicoRepository.java
+++ src/main/java/br/com/alura/forum/repository/TopicoRepository.java
@@ -8,4 +8,7 @@ import br.com.alura.forum.modelo.Topico;
 
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
 	List<Topico> findByCursoNome(String nomeCurso);
+	
+	//@Query("SELECT t FROM Topico t WHERE t.curso.nome = :nomeCurso")
+    //List<Topico> carregarPorNomeDoCurso(@Param("nomeCurso")(String nomeCurso);
 }
